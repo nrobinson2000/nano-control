e00eb21219853cfd7ad1756169ee669d70a6ca4e
Nathan Robinson <nrobinson2000@me.com>
2016-08-31 21:31:35 -0400

    Enabling travis ci at 21:31 of 31-08-2016

--- test.sh
+++ test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+echo "Installing Dependencies..."
+echo
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -  > /dev/null
 sudo apt-get install -y nodejs expect  > /dev/null
+echo "Installing particle-cli..."
+echo
 npm install -g particle-cli  > /dev/null
+echo "Installing platformio..."
+echo
 pip install -U platformio  > /dev/null
 export PARTICLE_PATH="!(which particle)" > /dev/null
+echo "Logging into particle-cli..."
+echo
 ./particle-cli-login.sh $email $pass $PARTICLE_PATH  > /dev/null
